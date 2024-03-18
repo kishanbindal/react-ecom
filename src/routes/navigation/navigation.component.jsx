d2d8da89fcf66083578ff9d21fc4794c9e17754b
kishanbindal <kishan.bindal@gmail.com>
2024-03-19 00:07:01 +0530

    Added Basic React Routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+import './navigation.styles.scss';
+
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+
+// <Fragment /> similar to ng-container in angular
+// Allows us to not have a structural 'parent' element similar to ng-container
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className="navigation">
+            <Link className="logo-container" to="/">
+                <CrwnLogo className="logo"></CrwnLogo>
+            </Link>
+            <div className="nav-links-container">
+                <Link className="nav-link" to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    )
+}
+
+export default Navigation
