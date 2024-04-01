d0147b2b3b854b3f035515384af72b66996c4471
kishanbindal <kishan.bindal@gmail.com>
2024-04-01 07:11:30 +0530

    Adding Config File

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAzWp5XrOAFpYXAkDDjMWk_coA3mtP0zXw",
+    authDomain: "react-ecom-db-d90bb.firebaseapp.com",
+    projectId: "react-ecom-db-d90bb",
+    storageBucket: "react-ecom-db-d90bb.appspot.com",
+    messagingSenderId: "889039167341",
+    appId: "1:889039167341:web:d58b79068202eeb521c464"
+};
